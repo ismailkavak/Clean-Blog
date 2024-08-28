1d81950ec65398601a261f285bb1df1ca53c1b19
ismailkavak <poyrazkvk@hotmail.com>
2024-08-28 12:36:33 +0300

    Used bcrypt

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt')
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
@@ -13,10 +14,17 @@ const UserSchema = new Schema({
   },
   password: {
     type: String,
-    
+    required: true
   }
-
 });
+
+UserSchema.pre('save', function(next){
+    const user = this;
+    bcrypt.hash(user.password, 10, (error, hash) => {
+        user.password = hash
+        next()
+    })
+})
 
 
 const User = mongoose.model("User", UserSchema);
