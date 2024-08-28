23205fbefdc18377ce048f260a8c887062f369fb
ismailkavak <poyrazkvk@hotmail.com>
2024-08-28 16:47:02 +0300

    set up session module

--- app.js
+++ app.js
@@ -1,27 +1,43 @@
 const express = require("express");
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+const session = require("express-session");
 const pageRoute = require("./routes/pageRoutes");
-const courseRoute = require('./routes/courseRoutes')
-const categoryRoute = require('./routes/categoryRoutes')
-const userRoute = require('./routes/userRoute')
+const courseRoute = require("./routes/courseRoutes");
+const categoryRoute = require("./routes/categoryRoutes");
+const userRoute = require("./routes/userRoute");
 
 const app = express();
 // CONNECT DB
-mongoose.connect('mongodb://127.0.0.1:27017/smartedu-db')
+mongoose.connect("mongodb://127.0.0.1:27017/smartedu-db");
 
 // TEMPLATE ENGINE
 app.set("view engine", "ejs");
 
+// GLOBAL VARIABLE
+global.userIN = null;
+
 // MIDDLEWARES
 app.use(express.static("public"));
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.set("trust proxy", 1); // trust first proxy
+app.use(
+  session({
+    secret: "my_keyboard_cat",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 // ROUTES
+app.use("*", (req, res, next) => {
+  userIN = req.session.userID;
+  next();
+});
 app.use("/", pageRoute);
-app.use("/courses", courseRoute)
-app.use("/categories", categoryRoute)
-app.use("/users", userRoute)
+app.use("/courses", courseRoute);
+app.use("/categories", categoryRoute);
+app.use("/users", userRoute);
 
 const port = 3000;
 app.listen(port, () => {
