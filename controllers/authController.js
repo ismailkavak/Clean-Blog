5c2c6b3ee9763e47cc62ec1b09c6fee46e5cda25
ismailkavak <poyrazkvk@hotmail.com>
2024-08-28 13:51:40 +0300

    user login router

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 exports.createUser = async (req, res) => {
   try {
@@ -7,6 +8,25 @@ exports.createUser = async (req, res) => {
       status: "success",
       user,
     });
+  } catch (error) {
+    res.status(400).json({
+      status: "failed",
+      error,
+    });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.status(400).send("There is no user!");
+    }
+    const same = await bcrypt.compare(password, user.password);
+    if(same){
+      res.status(200).send("You loggedd in!")
+    }
   } catch (error) {
     res.status(400).json({
       status: "failed",
