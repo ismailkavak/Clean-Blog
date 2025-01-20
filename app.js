e82fafed1f30fc8a75947f74f21f98772d08003d
ismailkavak <poyrazkvk@hotmail.com>
2025-01-20 23:24:23 +0300

    changed mongoose connection

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
-const flash = require('connect-flash');
-const methodOverride = require('method-override')
+const flash = require("connect-flash");
+const methodOverride = require("method-override");
 const pageRoute = require("./routes/pageRoutes");
 const courseRoute = require("./routes/courseRoutes");
 const categoryRoute = require("./routes/categoryRoutes");
@@ -11,7 +11,10 @@ const userRoute = require("./routes/userRoute");
 
 const app = express();
 // CONNECT DB
-mongoose.connect("mongodb+srv://casus589:sQUi37g6qGTIAbDx@cluster0.wdef7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+// mongoose.connect("mongodb+srv://casus589:sQUi37g6qGTIAbDx@cluster0.wdef7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+mongoose.connect(
+  "mongodb+srv://casus589:olHEC6PJJn7quqvk@cluster0.wdef7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+);
 
 // TEMPLATE ENGINE
 app.set("view engine", "ejs");
@@ -29,17 +32,22 @@ app.use(
     secret: "my_keyboard_cat",
     resave: false,
     saveUninitialized: true,
-    store: MongoStore.create({ mongoUrl: "mongodb+srv://casus589:sQUi37g6qGTIAbDx@cluster0.wdef7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" }),
+    store: MongoStore.create({
+      mongoUrl:
+        "mongodb+srv://casus589:olHEC6PJJn7quqvk@cluster0.wdef7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    }),
   })
 );
 app.use(flash());
 app.use((req, res, next) => {
   res.locals.flashMessages = req.flash();
   next();
-})
-app.use(methodOverride('_method', {
-  methods : ['POST', 'GET'],
-}))
+});
+app.use(
+  methodOverride("_method", {
+    methods: ["POST", "GET"],
+  })
+);
 
 // ROUTES
 app.use("*", (req, res, next) => {
